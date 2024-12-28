681687d1ea745b0899e2616ad1528a8e78722b81
Dagmand <zzar2010@gmail.com>
2024-12-28 15:49:09 +0000

    deleted:    num1 = int(input()).py 	modified:   "\320\236\320\261\321\200\320\260\320\261\320\276\321\202\320\272\320\260 \320\270\321\201\320\272\320\273\321\216\321\207\320\265\320\275\320\270\320\271.py"

--- Обработка исключений.py
+++ Обработка исключений.py
@@ -1,15 +1,14 @@
 #Обработка исключений
 
-result = 0
-
 try:
-    num1 = int(input('Введите первое число: '))
-    num2 = int(input('Введите второе число: '))
+    num1 = int(input("Введите первое число: "))
+    num2 = int(input("Введите второе число: "))
     result = num1 / num2
 except ValueError:
     print("Ошибка: введено не числовое значение!")
 except ZeroDivisionError:
     print("Ошибка: Деление на ноль!")
-finally:
+else:
     print(result)
-    
+finally:
+    print("Работа программы завершена")
