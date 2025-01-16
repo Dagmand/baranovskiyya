073cacf312d84a41ed09cc4d2bfbbb754913add2
Dagmand <zzar2010@gmail.com>
2025-01-16 17:43:13 +0000

    update

---
+++ ДЗ: Объявление и вызов функций.py
@@ -0,0 +1,11 @@
+def say_hello():
+    print("Hello, Python!")
+
+
+def repeat_message(message):
+    result = message + '\n'
+    print(result * 3)
+
+
+say_hello()
+repeat_message('Я не повторяюсь!')
---
+++ ДЗ: Функции с аргументами и возвращаемыми значениями.py
@@ -0,0 +1,7 @@
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+
+result = multiply(6, 7)
+
+print(result)
