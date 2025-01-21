880a7a2257d09e8932b52ba0d03ffb23a81c992e
Dagmand <zzar2010@gmail.com>
2025-01-21 19:24:18 +0000

    update

---
+++ ДЗ: Логические выражения и условия.py
@@ -0,0 +1,12 @@
+user_age = int(input('Введите Ваш возраст (полных лет): '))
+user_nationality = str(input('Являетесь ли Вы гражданином (Да или Нет): '))
+user_criminal_record = str(input('Имеется ли у Вас судимость (Да или Нет): '))
+
+age_status = 'достаточно' if user_age >= 18 else 'не достаточно'
+nationality_status = 'гражданин' if user_age >= 18 else 'не гражданин'
+criminal_status = 'есть судимость' if user_age >= 18 else 'нет судимости'
+
+decision = True if user_age >= 18 and user_nationality == 'Да' else False
+
+print(f'Вам {age_status} лет. Вы {nationality_status} страны. У Вас {criminal_status}.')
+print('Вы допущены до голосования' if decision == True else 'Вы не допущены до голосования')
