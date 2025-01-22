e3f320430db05aa22a925c9f46c5332b29b5e848
Dagmand <zzar2010@gmail.com>
2025-01-22 18:18:17 +0000

    update

--- ДЗ: Логические выражения и условия.py
+++ ДЗ: Логические выражения и условия.py
@@ -1,12 +1,13 @@
-user_age = int(input('Введите Ваш возраст (полных лет): '))
-user_nationality = str(input('Являетесь ли Вы гражданином (Да или Нет): '))
-user_criminal_record = str(input('Имеется ли у Вас судимость (Да или Нет): '))
-
-age_status = 'достаточно' if user_age >= 18 else 'не достаточно'
-nationality_status = 'гражданин' if user_nationality == 'Да' else 'не гражданин'
-criminal_status = 'есть судимость' if user_criminal_record == 'Да' else 'нет судимости'
-
-decision = True if user_age >= 18 and user_nationality == 'Да' else False
-
-print(f'Вам {age_status} лет. Вы {nationality_status} страны. У Вас {criminal_status}.')
-print('Вы допущены до голосования' if decision == True else 'Вы не допущены до голосования')
+try:
+    user_age = int(input('Введите Ваш возраст (полных лет): '))
+    user_nationality = str(input('Являетесь ли Вы гражданином (Да или Нет): '))
+    user_criminal_record = str(input('Имеется ли у Вас судимость (Да или Нет): '))
+except ValueError:
+    print('Ошибка: Введены не корректные данные')
+else:
+    if user_age >= 18 and user_nationality == 'Да' and user_criminal_record == 'Нет':
+        print('Вы допущены до голосования')
+    elif user_age < 18 or user_nationality == 'Нет' or user_criminal_record == 'Да':
+        print('Вы не допущены до голосования')
+    else:
+        print('Ошибка: Введены не корректные данные')
