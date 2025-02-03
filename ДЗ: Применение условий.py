7ba8212b74123ffa5a6279b6729d831aaa709598
Dagmand <zzar2010@gmail.com>
2025-02-03 13:16:14 +0000

    update

--- ДЗ: Применение условий.py
+++ ДЗ: Применение условий.py
@@ -2,26 +2,26 @@ def hundred_number_sum():
     hundred_number_sum = 0
     for number in range(1, 101):
         hundred_number_sum += number
-    print('Сумма всех цифр диапазона от 1 до 100 равна', hundred_number_sum, end='\n\n')
+    print('Сумма всех чисел диапазона от 1 до 100 равна', hundred_number_sum, end='\n\n')
 
 
 def user_input_numbers():
     list_input_number = []
     while True:
         try:
-            input_number = float(input('Введите цифру: '))
+            input_number = float(input('Введите число: '))
         except ValueError:
             print("Ошибка: введено не числовое значение!")
         else:
             if input_number >= 0:
                 list_input_number.append(input_number)
             else:
-                print ('Кол-во введёных цифр: ', len(list_input_number), end='\n\n')
+                print ('Кол-во введёных чисел: ', len(list_input_number), end='\n\n')
                 return False
 
 
 hundred_number_sum()
 
-print('Cписок содержащий квадраты всех нечетных чисел от 1 до 10: ', [num ** 2 for num in range(1, 11, 2)])
+print('Cписок содержащий квадраты всех нечетных чисел от 1 до 10: ', [num ** 2 for num in range(1, 11, 2)], end='\n\n')
 
 user_input_numbers()
