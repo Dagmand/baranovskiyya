2d863bcc5ba2c582b9d70a880b4f9c6c0077ffa6
Dagmand <zzar2010@gmail.com>
2025-03-26 17:40:11 +0000

    update

--- ДЗ: Сложение элементов списков.py
+++ ДЗ: Сложение элементов списков.py
@@ -1,17 +1,16 @@
-evens = [1, 3, 5, 7, 9, 10]
-odds = [0, 2, 4, 6, 8]
+evens = [1, 3, 5, 7, 9]
+odds = [0, 2, 4, 6, 8, 10]
 
-sum = []
+sum_of_numbers = []
 
 for i in range(max(len(evens), len(odds))):
     try:
-        sum.append(evens[i] + odds[i])
+        sum_of_numbers.append(evens[i] + odds[i])
     except IndexError:
-        if i >= min(len(evens), len(odds)):
-            if len(evens) == i + 1 and len(evens) < len(odds):
-                sum.append(odds[i])
-            else:
-                sum.append(evens[i])
+        if len(evens) < len(odds):
+            sum_of_numbers.append(odds[i])
+        else:
+            sum_of_numbers.append(evens[i])
 
 print(sum)
 
